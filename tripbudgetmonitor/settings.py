6bd485a62b3c277503deb2813ee1b2daadc1b46c
vamsee369 <vamseev213@gmail.com>
2025-12-04 15:15:12 +0530

    updated

--- tripbudgetmonitor/settings.py
+++ tripbudgetmonitor/settings.py
@@ -135,3 +135,6 @@ LOGIN_URL = '/accounts/login/'  # default redirect for @login_required
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # Collected static files for deployment
+STATICFILES_DIRS = [BASE_DIR / 'static']  # Additional static files directories
